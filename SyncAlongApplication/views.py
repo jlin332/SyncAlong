496f4b48573fe2e404d941bcc14edc0d6a6a9de3
John Lin <bigjohnlin@gmail.com>
2017-12-11 22:29:22 -0500

    Added all pages and global css

--- SyncAlongApplication/views.py
+++ SyncAlongApplication/views.py
@@ -9,3 +9,12 @@ from django.http import HttpResponse
 # Create your views here.
 def displayMySongs(request):
     return render(request, "MySongs.html", {})
+
+def displayUploadSongs(request):
+    return render(request, "UploadSongs.html", {})
+
+def displayCreatePlaylist(request):
+    return render(request, "CreatePlaylist.html", {})
+
+def displaUploadLyric(request):
+    return render(request, "UploadLyric.html", {})
